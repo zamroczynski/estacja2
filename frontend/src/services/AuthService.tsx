963ddd79c5756f90c123e1b9e9bb755ce3f6bda8
damian <damianzamroczynski@gmail.com>
2023-05-19 11:08:38 +0200

    Add AuthService.tsx

--- frontend/src/services/AuthService.tsx
+++ frontend/src/services/AuthService.tsx
@@ -1,22 +1,39 @@
-import API_URL from '../../config.json';
+import axios from "axios";
 
+import API_URL from "../../config.json";
 
 class AuthService {
-    login(username: string, password: string) {
-        console.log(API_URL);
-        if (username === password) {
-            localStorage.setItem("user", JSON.stringify('{"token": "ok"}'));
+  login(username: string, password: string) {
+    return axios
+      .post(API_URL + "login", {
+        username,
+        password,
+      })
+      .then((response) => {
+        if (response.data.accessToken) {
+          localStorage.setItem("user", JSON.stringify(response.data));
         }
-    }
+        return response.data;
+      });
+  }
 
-    logout() {
-        localStorage.removeItem("user");
-    }
+  logout() {
+    localStorage.removeItem("user");
+  }
 
-    getCurrentUser() {
-        const userStr = localStorage.getItem("user");
-        if (userStr) return JSON.parse(userStr);
-    }
+  register(username: string, password: string) {
+    return axios.post(API_URL + "register", {
+      username,
+      password,
+    });
+  }
+
+  getCurrentUser() {
+    const userStr = localStorage.getItem("user");
+    if (userStr) return JSON.parse(userStr);
+
+    return null;
+  }
 }
 
 export default new AuthService();
