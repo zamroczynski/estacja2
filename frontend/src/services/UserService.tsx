6c42067444b9bc382e82551e3a70a264481b7790
damian <damianzamroczynski@gmail.com>
2023-05-24 09:40:32 +0200

    Change login check api route

--- frontend/src/services/UserService.tsx
+++ frontend/src/services/UserService.tsx
@@ -1,22 +1,3 @@
-import axios from "axios";
-import authHeader from "./auth-header";
-
-const API_URL = "";
-
 class UserService {
-  getPublicContent() {
-    return axios.get(API_URL + "all");
-  }
-
-  getUserBoard() {
-    return axios.get(API_URL + "user", { headers: authHeader() });
-  }
-
-  getModeratorBoard() {
-    return axios.get(API_URL + "mod", { headers: authHeader() });
-  }
-
-  getAdminBoard() {
-    return axios.get(API_URL + "admin", { headers: authHeader() });
-  }
+  // TO DO
 }
