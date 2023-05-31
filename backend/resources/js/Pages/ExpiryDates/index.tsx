a09c790e82585798290c3e6c9e72a65245900802
damian <damianzamroczynski@gmail.com>
2023-05-31 13:35:14 +0200

    Add Tabs and TabPanels to ExpiryDates

--- backend/resources/js/Pages/ExpiryDates/index.tsx
+++ backend/resources/js/Pages/ExpiryDates/index.tsx
@@ -1,25 +1,66 @@
+import * as React from "react";
 import { Head } from "@inertiajs/react";
 import { PageProps } from "@/types";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { Box, Tab } from "@mui/material";
+import Tabs, { tabsClasses } from "@mui/material/Tabs";
+import TabPanel from "./Partials/TabPanel";
 
 export default function index({
     auth,
 }: PageProps<{ products: Array<string> }>) {
+    const [tabValue, setTabValue] = React.useState(0);
+    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+        setTabValue(newValue);
+    };
     return (
         <AuthenticatedLayout
             user={auth.user}
             header={
                 <h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-                    Mój profil
+                    Terminy
                 </h2>
             }
         >
             <Head title="Terminy" />
 
             <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-                    <div className="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                        Terminy
+                <div className="mx-auto sm:px-6 lg:px-8 space-y-6">
+                    <div className="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg ">
+                        <div className="flex justify-center items-center">
+                            <Tabs
+                                value={tabValue}
+                                onChange={handleChange}
+                                variant="scrollable"
+                                scrollButtons
+                                allowScrollButtonsMobile
+                            >
+                                <Tab label="Terminy" />
+                                <Tab label="Dodaj termin" />
+                                <Tab label="Dodaj Produkt" />
+                                <Tab label="Lista Produktów" />
+                                <Tab label="Zarządzaj" />
+                                <Tab label="Raport" />
+                            </Tabs>
+                        </div>
+                        <TabPanel value={tabValue} index={0}>
+                            Terminy
+                        </TabPanel>
+                        <TabPanel value={tabValue} index={1}>
+                            Dodaj
+                        </TabPanel>
+                        <TabPanel value={tabValue} index={2}>
+                            Dodaj prod
+                        </TabPanel>
+                        <TabPanel value={tabValue} index={3}>
+                            Produkty
+                        </TabPanel>
+                        <TabPanel value={tabValue} index={4}>
+                            Zarządzaj
+                        </TabPanel>
+                        <TabPanel value={tabValue} index={5}>
+                            Raport
+                        </TabPanel>
                     </div>
                 </div>
             </div>
