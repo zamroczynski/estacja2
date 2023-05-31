ba7b466ff2221b9d84929d9d2371466d5f31199f
damian <damianzamroczynski@gmail.com>
2023-05-31 09:43:19 +0200

    Setup React + Laravel

---
+++ backend/resources/js/Pages/Profile/Edit.tsx
@@ -0,0 +1,29 @@
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import UpdatePasswordForm from "./Partials/UpdatePasswordForm";
+import { Head } from "@inertiajs/react";
+import { PageProps } from "@/types";
+
+export default function Edit({
+    auth,
+}: PageProps<{ mustVerifyEmail: boolean; status?: string }>) {
+    return (
+        <AuthenticatedLayout
+            user={auth.user}
+            header={
+                <h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+                    Mój profil
+                </h2>
+            }
+        >
+            <Head title="Profile" />
+
+            <div className="py-12">
+                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+                    <div className="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+                        <UpdatePasswordForm className="max-w-xl" />
+                    </div>
+                </div>
+            </div>
+        </AuthenticatedLayout>
+    );
+}
