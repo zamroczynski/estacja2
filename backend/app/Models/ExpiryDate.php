10831af582e37994c68f7e37e8e31f8ccefcf23f
damian <damianzamroczynski@gmail.com>
2023-06-01 10:06:08 +0200

    Add CreatedUpdatedBy Trait

--- backend/app/Models/ExpiryDate.php
+++ backend/app/Models/ExpiryDate.php
@@ -6,10 +6,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Traits\CreatedUpdatedBy;
 
 class ExpiryDate extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory, SoftDeletes, CreatedUpdatedBy;
 
     /**
      * The attributes that are mass assignable.
