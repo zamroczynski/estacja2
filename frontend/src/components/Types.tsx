ff9a52e354e114675333218b6478b9d600b1a5d5
damian <damianzamroczynski@gmail.com>
2023-08-24 09:42:49 +0200

    Delete frontend folder and change name of main folder

--- frontend/src/components/Types.tsx
+++
@@ -1,7 +0,0 @@
-export enum Color {
-  Primary = "primary",
-  Secondary = "secondary",
-  Warning = "warning",
-  Info = "info",
-  Success = "success",
-}
--- frontend/src/pages/index.tsx
+++
@@ -1,4 +0,0 @@
-export { default as Home } from "./Home";
-export { default as ExpiryDates } from "./ExpiryDates";
-export { default as ErrorPage } from "./ErrorPage";
-export { default as Administration } from "./Administration";
